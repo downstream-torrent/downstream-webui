1d89a24821bb3be3655eeb1b1e4afabe882b4e18
Brendan Goodenough <brendan@goodenough.nz>
2017-07-29 12:11:28 +1200

    Move header to its own component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="header">
+    <h1 class="title">Downstream</h1>
+    <div class="left-toolbar">
+      <button><i class="fa fa-plus"></i></button>
+      <button><i class="fa fa-trash"></i></button>
+      <div class="button-separator"></div>
+      <button><i class="fa fa-pause"></i></button>
+      <button><i class="fa fa-play"></i></button>
+      <button><i class="fa fa-arrow-up"></i></button>
+      <button><i class="fa fa-arrow-down"></i></button>
+      <div class="button-separator"></div>
+      <button><i class="fa fa-magnet"></i></button>
+      <button><i class="fa fa-tv"></i></button>
+    </div>
+    <div class="right-toolbar">
+      <button><i class="fa fa-cog"></i></button>
+      <button><i class="fa fa-question"></i></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-component'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/styles.scss';
+
+.header {
+  display: flex;
+  width: 100%;
+  height: 60px;
+  background: $lighter-grey;
+  border-bottom: 1px solid $light-grey;
+  @include box-sizing;
+
+  .title {
+    width: 180px;
+    color: $medium-grey;
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 30px;
+    margin: 0;
+    padding: 15px;
+  }
+
+  .left-toolbar {
+    display: flex;
+    flex-grow: 1;
+    padding: 10px;
+  }
+
+  .right-toolbar {
+    padding: 10px;
+  }
+
+  button {
+    height: 40px;
+    padding: 0 20px;
+    background: $lightest-grey;
+    color: $dark-grey;
+    font-size: 16px;
+    border: 1px solid $light-grey;
+    border-radius: 5px;
+    outline: none;
+    cursor: pointer;
+
+    &:not(:first-child) {
+      margin-left: 5px;
+    }
+
+    &:hover {
+      background: $lighter-grey;
+    }
+  }
+
+  .button-separator {
+    margin-left: 15px;
+  }
+}
+</style>
--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="overview">
-    <div class="header">
-      <h1 class="title">Downstream</h1>
-      <div class="left-toolbar">
-        <button><i class="fa fa-plus"></i></button>
-        <button><i class="fa fa-trash"></i></button>
-        <div class="button-separator"></div>
-        <button><i class="fa fa-pause"></i></button>
-        <button><i class="fa fa-play"></i></button>
-        <button><i class="fa fa-arrow-up"></i></button>
-        <button><i class="fa fa-arrow-down"></i></button>
-        <div class="button-separator"></div>
-        <button><i class="fa fa-magnet"></i></button>
-        <button><i class="fa fa-tv"></i></button>
-      </div>
-      <div class="right-toolbar">
-        <button><i class="fa fa-cog"></i></button>
-        <button><i class="fa fa-question"></i></button>
-      </div>
-    </div>
+    <header-component></header-component>
     <div class="panels">
       <div class="left-sidebar">
         <div class="section">
@@ -67,6 +49,8 @@
 </template>
 
 <script>
+import HeaderComponent from '@/components/HeaderComponent'
+
 function select (torrent) {
   this.selected = Object.assign({}, this.selected, {
     [torrent.infoHash]: !this.selected[torrent.infoHash]
@@ -75,6 +59,9 @@ function select (torrent) {
 
 export default {
   name: 'overview',
+  components: {
+    HeaderComponent
+  },
   computed: {
     torrents () {
       return this.$store.getters.torrents
@@ -97,59 +84,6 @@ export default {
   flex-direction: column;
   width: 100%;
   height: 100%;
-}
-
-.header {
-  display: flex;
-  width: 100%;
-  height: 60px;
-  background: $lighter-grey;
-  border-bottom: 1px solid $light-grey;
-  @include box-sizing;
-
-  .title {
-    width: 180px;
-    color: $medium-grey;
-    font-size: 24px;
-    font-weight: 400;
-    line-height: 30px;
-    margin: 0;
-    padding: 15px;
-  }
-
-  .left-toolbar {
-    display: flex;
-    flex-grow: 1;
-    padding: 10px;
-  }
-
-  .right-toolbar {
-    padding: 10px;
-  }
-
-  button {
-    height: 40px;
-    padding: 0 20px;
-    background: $lightest-grey;
-    color: $dark-grey;
-    font-size: 16px;
-    border: 1px solid $light-grey;
-    border-radius: 5px;
-    outline: none;
-    cursor: pointer;
-
-    &:not(:first-child) {
-      margin-left: 5px;
-    }
-
-    &:hover {
-      background: $lighter-grey;
-    }
-  }
-
-  .button-separator {
-    margin-left: 15px;
-  }
 }
 
 .panels {
