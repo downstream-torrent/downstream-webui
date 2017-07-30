563f9fc0c99a66e2efac15dd3bf859155c5852a5
Brendan Goodenough <brendan@goodenough.nz>
2017-07-30 12:36:46 +1200

    Add ability to filter torrents in the sidebar by status

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -5,20 +5,30 @@
       <div class="left-sidebar">
         <div class="section">
           <h2 class="section-title">Torrent Status</h2>
-          <div class="item active"><i class="fa fa-circle-o"></i> All ({{torrents.length}})</div>
-          <div class="item" v-if="statuses.downloading"><i class="fa fa-arrow-circle-o-down"></i> Downloading ({{statuses.downloading}})</div>
-          <div class="item" v-if="statuses.seeding"><i class="fa fa-arrow-circle-o-up"></i> Seeding ({{statuses.seeding}})</div>
-          <div class="item" v-if="statuses.complete"><i class="fa fa-check-circle-o"></i> Complete ({{statuses.complete}})</div>
-          <div class="item" v-if="statuses.paused"><i class="fa fa-pause-circle-o"></i> Paused ({{statuses.paused}})</div>
-          <div class="item" v-if="statuses.stalled"><i class="fa fa-stop-circle-o"></i> Stalled ({{statuses.stalled}})</div>
-          <div class="item" v-if="statuses.error"><i class="fa fa-times-circle-o"></i> Error ({{statuses.error}})</div>
-        </div>
-        <div class="section">
-          <h2 class="section-title">Trackers</h2>
-          <div class="item active"><i class="fa fa-circle-o"></i> All</div>
+          <div class="item" @click="filter = null" :class="{ active: filter === null }">
+            <i class="fa fa-circle-o"></i> All ({{torrents.length}})
+          </div>
+          <div class="item" v-if="statuses.downloading" @click="filter = 'downloading'" :class="{ active: filter === 'downloading' }">
+            <i class="fa fa-arrow-circle-o-down"></i> Downloading ({{statuses.downloading}})
+          </div>
+          <div class="item" v-if="statuses.seeding" @click="filter = 'seeding'" :class="{ active: filter === 'seeding' }">
+            <i class="fa fa-arrow-circle-o-up"></i> Seeding ({{statuses.seeding}})
+          </div>
+          <div class="item" v-if="statuses.complete" @click="filter = 'complete'" :class="{ active: filter === 'complete' }">
+            <i class="fa fa-check-circle-o"></i> Complete ({{statuses.complete}})
+          </div>
+          <div class="item" v-if="statuses.paused" @click="filter = 'paused'" :class="{ active: filter === 'paused' }">
+            <i class="fa fa-pause-circle-o"></i> Paused ({{statuses.paused}})
+          </div>
+          <div class="item" v-if="statuses.stalled" @click="filter = 'stalled'" :class="{ active: filter === 'stalled' }">
+            <i class="fa fa-stop-circle-o"></i> Stalled ({{statuses.stalled}})
+          </div>
+          <div class="item" v-if="statuses.error" @click="filter = 'error'" :class="{ active: filter === 'error' }">
+            <i class="fa fa-times-circle-o"></i> Error ({{statuses.error}})
+          </div>
         </div>
       </div>
-      <torrent-list :torrents="torrents"></torrent-list>
+      <torrent-list :torrents="filteredTorrents"></torrent-list>
     </div>
     <footer-component></footer-component>
   </div>
@@ -44,6 +54,9 @@ export default {
     TorrentList,
     FooterComponent
   },
+  data: () => ({
+    filter: null
+  }),
   created: function () {
     this.onPaste = onPaste.bind(this)
     document.addEventListener('paste', this.onPaste)
@@ -52,6 +65,9 @@ export default {
     document.removeEventListener('paste', this.onPaste)
   },
   computed: {
+    filteredTorrents () {
+      return this.$store.getters.torrents.filter(t => this.filter ? t.status === this.filter : t)
+    },
     torrents () {
       return this.$store.getters.torrents
     },
@@ -93,11 +109,10 @@ export default {
 }
 
 .left-sidebar {
-  width: 180px;
+  width: 200px;
   background: $lightest-grey;
   flex-shrink: 0;
   border-right: 1px solid $light-grey;
-  padding: 15px;
 
   .section {
     margin-bottom: 15px;
@@ -105,21 +120,27 @@ export default {
     .section-title {
       color: $medium-grey;
       font-weight: 700;
-      font-size: 18px;
+      font-size: 16px;
       margin: 0;
+      padding: 15px;
     }
 
     .item {
+      height: 50px;
       color: $medium-grey;
       font-size: 16px;
-      margin-top: 5px;
+      padding: 0 15px;
+      cursor: pointer;
+      line-height: 50px;
 
-      &:hover {
-        color: $darker-grey;
+      i {
+        font-size: 18px;
+        padding-right: 5px;
       }
 
       &.active {
-        color: $blue;
+        color: $darker-grey;
+        background: $light-grey;
       }
     }
   }
